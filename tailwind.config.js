5fb747398a6b825c69cfb88fa052ef4f917b16f4
arnabw <arnabsahawrk@gmail.com>
2024-04-01 08:49:40 +0600

    few configaration changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+import daisyui from "./node_modules/daisyui";
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -18,5 +19,8 @@ export default {
       },
     },
   },
-  plugins: [require("daisyui")],
+  plugins: [daisyui],
+  daisyui: {
+    themes: ["light"],
+  },
 };
